8ebbf2eb740fc52ba0e23e2b84fb179752af10fc
Giovanni Vargas <egiovanni.vo@gmail.com>
2022-12-21 14:10:12 -0600

    Version 2.0.0

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import uvicorn
 from fastapi import FastAPI
-import requests
+
+import aiohttp
+import asyncio
 
 app = FastAPI()
 
+async def peticion(url, client):
+    async with client.get(url) as r:
+        res = await r.json()
+        return res
+
+async def peticiones(urls):
+    async with aiohttp.ClientSession() as client:
+        responses = await asyncio.gather(*[peticion(url, client) for url in urls])
+        return responses
 
 @app.get("/jokes")
-async def root():
-    jokes = []
-    ids = []
+def root():
+    async_data = asyncio.run(peticiones(['https://api.chucknorris.io/jokes/random']*25))
+    jokes = [joke['value'] for joke in async_data]
+    ids = [joke['id'] for joke in async_data]
+    
     while len(jokes) != 25:
         res = requests.get('https://api.chucknorris.io/jokes/random')
         data = res.json()
